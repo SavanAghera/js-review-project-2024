161b9e7bdb615b6b736c6a0928049e40f6f663f9
pushti <pushti.soni@rapidops.co>
2024-04-06 14:17:04 +0530

    added task

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,23 +1,25 @@
 class SideBar extends BaseTable {
     getRenderData() {
         const categories = [];
-        tableData.forEach((data)=>{
-            const categoryType=types.find(
-                (category)=>category.name===data.category
-            )
-            if(categoryType){
-                categoryType.count++;
-                categoryType.price+=data.price;
+
+        const sideBarData = [];
+        for (let i = 0; i < tableData.length; i++) {
+            const row = tableData[i];
+            const existingRow = sideBarData.findIndex(data => data.name === row.category);
+            if (existingRow === -1) {
+                sideBarData.push({
+                    name: row.category,
+                    count: 1,
+                    price: row.price,
+                });
             }
             else{
-                categories.push({
-                    name:data.category,
-                    count:1,
-                    price:data.price
-                })
+                sideBarData[existingRow].count++;
+                sideBarData[existingRow].price+= row.price;
             }
-        });
-        return categories;
+
+        }
+        return sideBarData;
     }
     getCellData(data) {
         return [
@@ -25,11 +27,12 @@ class SideBar extends BaseTable {
             {text:data.count, elementName:'td', handleClick: ''},
             {text:data.price, elementName:'td', handleClick: ''},
         ]
+ 
     }
     listenEvent() {
         document.addEventListener('delete', () => this.render())
+        document.addEventListener('delete', () => this.render());
+        document.addEventListener('submit', () => this.render());
     }
 
 }
-
-
--- components/table.js
+++ components/table.js
@@ -6,8 +6,14 @@ class Table extends BaseTable{
         return [
             {text:data.productName, elementName:'td', handleClick:''},
             {text:data.category, elementName:'td', handleClick: ''},
-            {text: data.price, elementName: 'td', handleClick: '' }
+            {text:data.price, elementName:'td', handleClick:''},
+            {text:'delete',elementName:'button', handleClick:()=>this.deleteRow(data)}
         ]
     }
+    deleteRow(data){
+       const index=tableData.indexOf(data);
+       tableData.splice(index,1);
+       this.render();
+       document.dispatchEvent(new Event('delete'))
+    }
 }
-
