90119d5324ff0b27eb0bb24e47b25f993b55258b
yashpatel-rapidops <yash.patel@rapidops.co>
2024-04-10 11:58:39 +0530

    task completed

--- components/table.js
+++ components/table.js
@@ -1,29 +1,28 @@
-class Table extends BaseTable{
-    getRenderData() {
-        return tableData
-    }
-    getCellData(data) {
-        return [
-            {text:data.productName, elementName:'td', handleClick:''},
-            {text:data.category, elementName:'td', handleClick: ''},
-            {text:data.price,elementName:'td',handleClick:''},
-            {text:"delete",elementName:'button',handleClick:super.deleteRow},
-        ]
-    }
-    deleteRow=function(e){
-        // let deleteProductName=e.target.parentElement.firstChild.textContent;
-        // e.target.parentElement.remove();
-        // let obj=tableData.find((ele)=>{
-        //     return ele.productName===deleteProductName
-        // })
-        // tableData.splice(tableData.indexOf(obj),1);
-        // console.log("deletd ",obj);
-
-    }
-    listenEvent(){
-        document.addEventListener('reRenderTable',(e)=>{
-            console.log('render table',this);
-            this.render();
-        })
-    }
+class Table extends BaseTable {
+  getRenderData() {
+    return tableData;
+  }
+  getCellData(data) {
+    return [
+      { text: data.productName, elementName: "td", handleClick: "" },
+      { text: data.category, elementName: "td", handleClick: "" },
+      { text: data.price, elementName: "td", handleClick: "" },
+      { text: "delete", elementName: "button", handleClick: super.deleteRow },
+    ];
+  }
+  deleteRow = function (e) {
+    // let deleteProductName=e.target.parentElement.firstChild.textContent;
+    // e.target.parentElement.remove();
+    // let obj=tableData.find((ele)=>{
+    //     return ele.productName===deleteProductName
+    // })
+    // tableData.splice(tableData.indexOf(obj),1);
+    // console.log("deletd ",obj);
+  };
+  listenEvent() {
+    document.addEventListener("reRenderTable", (e) => {
+      console.log("render table", this);
+      this.render();
+    });
+  }
 }
